b2247f867e2dff77d6425edc62bf5e43e5375b59
gzydong <837215079@qq.com>
2022-01-04 22:47:28 +0800

    feat: 功能开发

--- provider/server.go
+++ provider/server.go
@@ -13,24 +13,19 @@ type WebsocketServer *http.Server
 func NewHttpServer(conf *config.Config, handler *gin.Engine) *http.Server {
 
 	// 默认处理端口号
-	if conf.Server.Port == 0 {
-		conf.Server.Port = 8080
-	}
+	// if conf.Server.Port == 0 {
+	// 	conf.Server.Port = 8080
+	// }
 
 	return &http.Server{
-		Addr:    fmt.Sprintf("0.0.0.0:%d", conf.Server.Port),
+		Addr:    fmt.Sprintf("0.0.0.0:%d", 9503),
 		Handler: handler,
 	}
 }
 
-func NewWebsocketServer(conf *config.Config, handler *gin.Engine) WebsocketServer {
-	// 默认处理端口号
-	if conf.Server.Port == 0 {
-		conf.Server.Port = 8081
-	}
-
+func NewWebsocketServer(conf *config.Config, handler *gin.Engine) *http.Server {
 	return &http.Server{
-		Addr:    fmt.Sprintf("0.0.0.0:%d", conf.Server.Port),
+		Addr:    fmt.Sprintf("0.0.0.0:%d", 9504),
 		Handler: handler,
 	}
 }
