f9708c362037abbb74c04973611ffc97becdb449
yuandong.rao <Yuandong.Rao@verystar.cn>
2023-03-28 18:34:18 +0800

    feat:代码优化

--- internal/pkg/utils/util.go
+++ internal/pkg/utils/util.go
@@ -5,11 +5,12 @@ import (
 	"fmt"
 	"math/rand"
 	"runtime"
+	"time"
 )
 
 // MtRand 生成指定范围内的随机数
 func MtRand(min, max int) int {
-	r := rand.New(rand.NewSource(1))
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return r.Intn(max-min+1) + min
 }
 
