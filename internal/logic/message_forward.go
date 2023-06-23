90a6eaf3a416123f19a4cecfb0614c1711c2d411
gzydong <837215079@qq.com>
2023-06-23 19:40:21 +0800

    feat:代码优化

--- internal/logic/message_forward.go
+++ internal/logic/message_forward.go
@@ -42,7 +42,7 @@ func (m *MessageForwardLogic) Verify(ctx context.Context, uid int, req *message.
 	}
 
 	query.Where("talk_type = ?", req.Receiver.TalkType)
-	query.Where("msg_type in ?", []int{1, 2, 3, 4, 5, 6, 7, 8})
+	query.Where("msg_type in ?", []int{1, 2, 3, 4, 5, 6, 7, 8, entity.ChatMsgTypeForward})
 	query.Where("is_revoke = ?", 0)
 
 	var count int64
