2834e32f603838b3202e11fbecce9b7a863a5272
gzydong <837215079@qq.com>
2023-12-10 20:59:40 +0800

    feat:优化代码

--- internal/job/temp/test.go
+++ internal/job/temp/test.go
@@ -3,10 +3,7 @@ package temp
 import (
 	"github.com/urfave/cli/v2"
 	"go-chat/config"
-	"go-chat/internal/pkg/jsonutil"
-	"go-chat/internal/repository/model"
 	"go-chat/internal/repository/repo"
-	"gorm.io/gorm"
 )
 
 type TestCommand struct {
@@ -16,33 +13,33 @@ type TestCommand struct {
 
 func (t *TestCommand) Run(ctx *cli.Context, conf *config.Config) error {
 
-	var i int32
-
-	for {
-		info, err := t.TalkRecordsRepo.FindAll(ctx.Context, func(db *gorm.DB) {
-			db.Where("id > ?", i)
-			db.Where("msg_type in (?)", []int{1, 1000})
-			db.Limit(1000)
-		})
-
-		if err != nil {
-			return err
-		}
-
-		for _, v := range info {
-			t.TalkRecordsRepo.UpdateWhere(ctx.Context, map[string]any{
-				"extra": jsonutil.Encode(model.TalkRecordExtraText{
-					Content: v.Content,
-				}),
-			}, "id = ?", v.Id)
-		}
-
-		if len(info) < 1000 {
-			break
-		}
-
-		i = int32(info[len(info)-1].Id)
-	}
+	// var i int32
+	//
+	// for {
+	// 	info, err := t.TalkRecordsRepo.FindAll(ctx.Context, func(db *gorm.DB) {
+	// 		db.Where("id > ?", i)
+	// 		db.Where("msg_type in (?)", []int{1, 1000})
+	// 		db.Limit(1000)
+	// 	})
+	//
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	//
+	// 	for _, v := range info {
+	// 		t.TalkRecordsRepo.UpdateWhere(ctx.Context, map[string]any{
+	// 			"extra": jsonutil.Encode(model.TalkRecordExtraText{
+	// 				Content: v.Content,
+	// 			}),
+	// 		}, "id = ?", v.Id)
+	// 	}
+	//
+	// 	if len(info) < 1000 {
+	// 		break
+	// 	}
+	//
+	// 	i = int32(info[len(info)-1].Id)
+	// }
 
 	return nil
 }
